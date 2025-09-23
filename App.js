595e44bc6861a7f8825fe7691cc100e38a5e831c
iamusicclub <scottcee01@googlemail.com>
2025-09-23 15:04:17 +0100

    Update App.js

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import { db } from "./firebase";
 import NominateAlbum from "./NominateAlbum";
 import AlbumList from "./AlbumListNew";
 import ScheduleViewer from "./ScheduleViewer";
-import GenerateSchedule from "./GenerateSchedules;
+import GenerateSchedule from "./GenerateSchedules";
 
 function App() {
   const [user, setUser] = useState(null);
