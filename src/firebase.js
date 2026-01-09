ac3b20fff4e11b81c310b12b840633e6d890a83e
iamusicclub <scottcee01@googlemail.com>
2026-01-09 15:19:15 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// ✅ Your config goes here (keep your existing values)
 const firebaseConfig = {
-  apiKey: "AIzaSyAopvRqdvFLqtN-3UKsuYq1JRIaW8qYgN4",
-  authDomain: "ia-album-club.firebaseapp.com",
-  projectId: "ia-album-club",
-  storageBucket: "ia-album-club.firebasestorage.app",
-  messagingSenderId: "489116766362",
-  appId: "1:489116766362:web:0945749c39cc715dda0f23",
+  apiKey: "XXX",
+  authDomain: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXX",
+  appId: "XXX",
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
