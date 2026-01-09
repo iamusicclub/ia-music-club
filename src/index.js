16cccf685578d764c23153e177ce4447a97918c6
iamusicclub <scottcee01@googlemail.com>
2026-01-09 15:19:33 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
+import "./styles.css";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(rootElement).render(<App />);
